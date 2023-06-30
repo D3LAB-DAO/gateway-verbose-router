478c581e4f026836159e56563c49911a4c57c7c5
김민기 <lupincoinplug.com@gimmingiui-MacBookPro.local>
2023-06-30 17:13:26 +0900

    chore: Updated project id

--- scripts/setV8S.js
+++ scripts/setV8S.js
@@ -7,7 +7,7 @@ const v8sAddr = require("../config/contractAddrs.json").v8s;
 async function main() {
   const verboseRouter = await ethers.getContractAt("VerboseRouter", "0x17760E8C32BC6AB8907fBE5568D88D67E7386EDb");
 
-  const tx = await verboseRouter.setV8S(v8sAddr, 3);
+  const tx = await verboseRouter.setV8S(v8sAddr, 6);
   await tx.wait();
 
   console.log("tx hash: ", tx.hash);
@@ -19,4 +19,4 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
-// tx hash:  0x4a7602a30747590709a22190791e34c3ec0f183871877350e8037cb0fcdc320e
+// tx hash:  0x80547be158edae8c1e1c9fa69e5a15c65af13f22f24c599b00931a59a9afe772
